c359b3a6e82029d2be334db2ebc375c5d10558e2
Jeffrey Lau <jeffrey.lau@ribose.com>
2017-07-19 11:08:14 +0800

    Fix Rails 5.0 deprecation warnings for 5.1+

--- lib/devise_security_extension/rails.rb
+++ lib/devise_security_extension/rails.rb
@@ -3,8 +3,8 @@ module DeviseSecurityExtension
     ActiveSupport.on_load(:action_controller) do
       include DeviseSecurityExtension::Controllers::Helpers
     end
-    
-    if Rails.version > "5"
+
+    if Rails.version >= "5"
       ActiveSupport::Reloader.to_prepare do
         DeviseSecurityExtension::Patches.apply
       end
